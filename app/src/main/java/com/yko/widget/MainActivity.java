3941de11210ce5d80083ac93948d05be81c2b375
赖培忠 <lynyko@163.com>
2017-05-23 17:17:48 +0800

    update

--- app/src/main/java/com/yko/widget/MainActivity.java
+++ app/src/main/java/com/yko/widget/MainActivity.java
@@ -2,6 +2,10 @@ package com.yko.widget;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.yko.ykodialog.YkoMessageDialog;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new YkoMessageDialog.Builder(this).setTitle("测试标题")
+                .setLeftButton("我要取消", null)
+                .setRightButton("我要确认", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(MainActivity.this, "点击确认", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setMessage("测试信息测试信息测试信息测试信息测试信息测试信息测试信息测试信息测试信息测试信息测试信息测试信息测试信息测试信息测试信息测试信息测试信息测试信息测试信息测试信息")
+                .setGravity(YkoMessageDialog.Builder.GRAVITY_BOTTOM)
+                .setmOutsideFocusable(false)
+                .buidle().show();
     }
 }
